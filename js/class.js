70b2f08d8269bc35ed9dc37d1bbfd65862ca8837
OnuSaki <9180266@esmad.ipp.pt>
2019-12-10 09:52:51 +0000

    a

--- js/class.js
+++ js/class.js
@@ -1,9 +1,17 @@
-export default class Rect{
+export class Rect{
     constructor(id, xRect, yRect, L1, L2){
         this.id = id
-        this.xRect = xRect,
-        this.yRect = yRect,
+        this.xRect = xRect
+        this.yRect = yRect
         this.L1 = L1
         this.L2 = L2
+    }
+}
+
+export class Spot{
+    constructor(id, xSpot, ySpot){
+        this.id = id
+        this.xSpot = xSpot
+        this.ySpot = ySpot
     }
 }
